0a9d09e9f7a32598a05048f98757fc630e5caeb3
CaptainHook <126789811+C4ptainHook@users.noreply.github.com>
2024-01-28 15:22:42 +0200

    Delete Table data directory

--- Table data/activity.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO activity (objective, description, organizer,start_date, end_date)
-VALUES('Test activity', 'Only for testing purposes', 'Testing Department', DATE'15-01-2023', DATE'16-01-2024');
